6533b9b24b1e4036ffebc68e87b47858a8d66ff4
Lonerr7 <krabik2342@gmail.com>
2021-12-01 10:05:38 +0300

    Added dynamic rendering of ExpenseItem, added keys to fix an error with rendering and added rendering of filtered items

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
@@ -6,37 +6,29 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  const [enteredYear, setEnteredYear] = useState('2020');
+  const [enteredYear, setEnteredYear] = useState("2020");
 
   const saveYearHandler = (selectedYear) => {
-    console.log(`In Expenses.js ${selectedYear}`);
     setEnteredYear(selectedYear);
   };
+
+  const filteredExpense = props.items.filter(expense => expense.date.getFullYear().toString() === enteredYear);
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter startingYear={enteredYear} onChageFilter={saveYearHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+        <ExpensesFilter
+          startingYear={enteredYear}
+          onChageFilter={saveYearHandler}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {filteredExpense.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
